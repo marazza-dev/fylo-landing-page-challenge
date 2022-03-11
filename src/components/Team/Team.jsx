ca0451f413705fd0355ef5d7325528fb5cef137e
Federico Marazzani <marazzani.dev@gmail.com>
2022-03-11 12:13:53 +0100

    add Team component

---
+++ src/components/Team/Team.jsx
@@ -0,0 +1,33 @@
+import "./team.scss";
+
+import productiveImg from "../../images/illustration-stay-productive.png";
+import { FaArrowCircleRight } from "react-icons/fa";
+
+const Team = () => {
+  return (
+    <section className='team__container'>
+      <div className='team__image--wrapper'>
+        <img
+          src={productiveImg}
+          alt='people collaborating to be more productive'
+        />
+      </div>
+      <div className='team__content--wrapper'>
+        <h2 className='heading__sub'>Stay productive, wherever you are</h2>
+        <p className='paragraph'>
+          Never let location be an issue when accessing your files. Fylo has you
+          covered for all of your file storage needs.
+        </p>
+        <p className='paragraph'>
+          Securely share files and folders with friends, family and colleagues
+          for live collaboration. No email attachments required.
+        </p>
+        <a href='#' className='team__link'>
+          See how Fylo works <FaArrowCircleRight />
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Team;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Hero } from "./Hero/Hero";
 export { default as Features } from "./Features/Features";
+export { default as Team } from "./Team/Team";
